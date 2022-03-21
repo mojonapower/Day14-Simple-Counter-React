9399fc86c5006f3f11264fe9f3677606bd600f99
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-21 20:01:38 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
+/* import './components.jsx'; */
 import './App.css';
 
+const Contador = props => {
+  return(
+  <p>{props.tiempo}</p>
+  )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <Contador tiempo="0" />
+ )
 }
 
 export default App;
